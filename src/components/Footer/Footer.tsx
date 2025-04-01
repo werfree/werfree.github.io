3f36367b91dd932a9dbaaad531fe881b410ea008
Sayantan Ghosh <gsayantan01@gmail.com>
2025-04-02 04:50:47 +0530

    add height to footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <footer className="font-body text-sm color-body my-3">
+    <footer className="font-body h-fit text-sm color-body my-3">
       &copy;{new Date().getFullYear()}, All Rights Reserved
     </footer>
   );
